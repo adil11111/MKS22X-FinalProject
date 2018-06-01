32fae2e49be8380dcb6baa3c1651b592db48fe77
Adil Gondal <agondal@stuy.edu>
2018-06-01 14:46:15 -0400

    made drop function

--- Tile.java
+++ Tile.java
@@ -9,8 +9,21 @@ public class Tile {
         x = nx;
         y = ny;
     }
-
     public Tile(int nx, int ny) {
         this(nx, ny, (int) (Math.random()*7));
+    }
+    
+    public int getColor() {
+        return color;
+    }
+    
+    public boolean drop(TetrisBoard board) {
+        y++;
+        if (y >= 22 || board.getTile(x, y) != null) {
+            y--;
+            board.addTile(x, y, this);
+            return true; // true means locked in place
+        }
+        return false;
     }
 }
